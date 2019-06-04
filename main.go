f6846001a080c7fb7e202c4ed65da6ce52a22668
Guillermo Di Pasquale <guilledipasquale@gmail.com>
2019-06-04 22:23:16 +1000

    Tests and normalize function

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 // https://github.com/gophercises/phone
 package main
 
-func normalize(phone string) string {
+import (
+	"bytes"
+	"unicode"
+)
 
+func normalize(phone string) string {
+	var buf bytes.Buffer
+	for _, ch := range phone {
+		if unicode.IsNumber(ch) {
+			buf.WriteRune(ch)
+		}
+	}
+	return buf.String()
 }
 
 func main() {
